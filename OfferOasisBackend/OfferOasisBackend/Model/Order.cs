048940b1a164849084e122a5bcd63d9c4d677567
rekamakadi <r.makadi@gmail.com>
2023-09-12 14:23:10 +0200

    feat: create Order.cs

---
+++ OfferOasisBackend/OfferOasisBackend/Model/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace OfferOasisBackend.Model;
+
+public class Order
+{
+    public Guid Id { get; init; }
+    private List<Product> _products;
+    private string BillingAddress { get; init; }
+    private string ShippingAddress { get; init; }
+    private string ShippingType { get; init; } // Todo: lehetne enum
+    private double ShippingCost { get; init; }
+    private DateTime OrderDate { get; init; }
+    private User _user;
+}
