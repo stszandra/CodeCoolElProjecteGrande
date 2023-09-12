f09d9bfe03b662020b2d0c1f33b27ba83685b8c7
stszandra <snszandra@gmail.com>
2023-09-12 14:22:34 +0200

    feature: create ProductType enum

---
+++ OfferOasisBackend/OfferOasisBackend/Model/ProductType.cs
@@ -0,0 +1,14 @@
+﻿namespace OfferOasisBackend.Models;
+
+public enum ProductType
+{
+    Notebook,
+    Alaplap,
+    Processzor,
+    Videókártya,
+    SSD,
+    Memória,
+    Tápegység,
+    Monitor,
+    Konzol
+}
