654a241eed5b42f6c6d046478d98cbedf3c445d4
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-12 13:13:48 +0200

    feat: add User.cs base class and 3 subclasses (todo: mail validation and methods)

--- OfferOasisBackend/OfferOasisBackend/Model/User.cs
+++ OfferOasisBackend/OfferOasisBackend/Model/User.cs
@@ -1,6 +1,34 @@
 ﻿namespace OfferOasisBackend.Model;
 
-public class User
+public abstract class User
 {
+    // - id
+    // - email
+    // - password
+    // - firstname
+    // - lastname
+    // - address
     
+    // - shoppingCart (user)
+    // - previousOrders (user)
+    // - ratings List<Rating> (user)
+    
+    private Guid Id { get; }
+    private string FirstName { get; }
+    private string LastName { get; }
+    private string Address { get; }
+    private string Password { get; }
+    
+    // TODO: email with validation
+
+    protected User(Guid id, string firstName, string lastName, string address, string password)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        Address = address;
+        Password = password;
+    }
+    
+    // ... common methods?
 }
---
+++ OfferOasisBackend/OfferOasisBackend/Model/adminUser.cs
@@ -0,0 +1,10 @@
+﻿namespace OfferOasisBackend.Model;
+
+public class adminUser : User
+{
+    public adminUser(Guid id, string firstName, string lastName, string address, string password) : base(id, firstName, lastName, address, password)
+    {
+    }
+    
+    // ... subclass-specific methods
+}
