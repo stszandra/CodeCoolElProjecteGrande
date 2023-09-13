cfa794415e7eab882c54f4c133c5f48969c41516
rekamakadi <r.makadi@gmail.com>
2023-09-13 10:55:15 +0200

    feat: create OrderRepository.cs

---
+++ OfferOasisBackend/OfferOasisBackend/Service/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using OfferOasisBackend.Model;
+
+namespace OfferOasisBackend.Service;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly List<string> _orders = new();
+    
+    public Order? GetById(int id)
+    {
+        return null;
+    }
+
+    public HashSet<Order> GetAll()
+    {
+        return null;
+    }
+
+    public void Add(Order entity)
+    {
+        
+    }
+
+    public void Remove(int id)
+    {
+       
+    }
+}
