6a711510e976aa8f27861f25467d5656794d98f1
rekamakadi <r.makadi@gmail.com>
2023-09-13 09:56:44 +0200

    feat: create IGenericRepository.cs

---
+++ OfferOasisBackend/OfferOasisBackend/Service/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace OfferOasisBackend.Service;
+
+public interface IGenericRepository<T> where T : class
+{
+    T? GetById(int id);
+    HashSet<T> GetAll();
+    void Add(T entity);
+    void Remove(int id);
+}
