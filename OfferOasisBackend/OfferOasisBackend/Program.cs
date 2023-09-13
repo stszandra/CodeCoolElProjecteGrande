3823d37ab10f244c01e89e2b695c76cf8cc56933
stszandra <snszandra@gmail.com>
2023-09-13 11:14:53 +0200

    feature: add ProductRepository instance as Transient

--- OfferOasisBackend/OfferOasisBackend/Program.cs
+++ OfferOasisBackend/OfferOasisBackend/Program.cs
@@ -1,3 +1,5 @@
+using OfferOasisBackend.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
