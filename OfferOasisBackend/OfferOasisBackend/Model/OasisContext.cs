b8bcd0f2426c90319e0b3d4e4d047b2db2bcf5e7
Varga Armand <vargamondi@gmail.com>
2023-09-26 11:52:44 +0200

    features:Create OasisContext.cs and include tables for product and order

---
+++ OfferOasisBackend/OfferOasisBackend/Model/OasisContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfferOasisBackend.Models;
+
+namespace OfferOasisBackend.Model;
+
+public class OasisContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=WeatherApi;User Id=sa;Password=Kiskutyafüle32!");
+        //trust credentials
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+            .HasIndex(u => u.Id)
+            .IsUnique();
+        
+        builder.Entity<Order>()
+            .HasIndex(o => o.Id)
+            .IsUnique();
+        
+        builder.Entity<Product>();
+        builder.Entity<Order>();
+
+    }
+}
