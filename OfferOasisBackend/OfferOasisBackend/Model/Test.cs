c2e775788d439aa51c2f859cb0f8df2cdeaa895b
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-27 11:00:54 +0200

    feat: create test class for checking DB connection capabilities

---
+++ OfferOasisBackend/OfferOasisBackend/Model/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace OfferOasisBackend.Model;
+
+public class Test
+{
+    public int Id { get; init; }
+    public string StringTest { get; set; }
+    public int IntTest { get; set; }
+
+    public Test(string stringTest, int intTest)
+    {
+        StringTest = stringTest;
+        IntTest = intTest;
+    }
+}
