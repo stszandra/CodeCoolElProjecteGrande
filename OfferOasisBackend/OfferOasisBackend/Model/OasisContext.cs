7f608f114c88b6e33ea23addc7f4d52c406acfcf
rekamakadi <r.makadi@gmail.com>
2023-09-27 14:38:50 +0200

    feat: accommodate test requirements

--- OfferOasisBackend/OfferOasisBackend/Model/OasisContext.cs
+++ OfferOasisBackend/OfferOasisBackend/Model/OasisContext.cs
@@ -5,9 +5,9 @@ namespace OfferOasisBackend.Model;
 
 public class OasisContext : DbContext
 {
-    public DbSet<Product> Products { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<Rating> Ratings { get; set; }
+    // public DbSet<Product> Products { get; set; }
+    // public DbSet<Order> Orders { get; set; }
+    // public DbSet<Rating> Ratings { get; set; }
 
     public DbSet<Test> Test {get; set; }
     
@@ -16,35 +16,24 @@ public class OasisContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
-            "Server=localhost,1433;Database=test3;User Id=sa;Password=Kiskutyafüle32!;TrustServerCertificate=True");
-        //trust credentials
+            "Server=localhost,1433;Database=testDB;User Id=sa;Password=Kiskutyafüle32!;TrustServerCertificate=True");
     }
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.Entity<Product>()
-            .HasIndex(u => u.Id)
-            .IsUnique();
-        
-        builder.Entity<Order>()
-            .HasIndex(o => o.Id)
-            .IsUnique();
-        
-        builder.Entity<Product>();
-        builder.Entity<Order>();
+        // builder.Entity<Product>()
+        //     .HasIndex(u => u.Id)
+        //     .IsUnique();
+        //
+        // builder.Entity<Order>()
+        //     .HasIndex(o => o.Id)
+        //     .IsUnique();
+        //
+        // builder.Entity<Rating>()
+        //     .HasIndex(r => r.Id)
+        //     .IsUnique();
         
         builder.Entity<Test>()
             .HasIndex(t => t.Id)
             .IsUnique();
-        
-        builder.Entity<Test>();
-        
-        builder.Entity<Rating>()
-            .HasIndex(r => r.Id)
-            .IsUnique();
-        
-        builder.Entity<Rating>();
-        
-        
-
     }
 }
