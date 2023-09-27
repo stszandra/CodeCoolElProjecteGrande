b15e0e8367b8f902d4aa9b420a6dc8c4f8a90d28
rekamakadi <r.makadi@gmail.com>
2023-09-27 14:45:36 +0200

    feat: create TestController.cs

---
+++ OfferOasisBackend/OfferOasisBackend/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfferOasisBackend.Model;
+using OfferOasisBackend.Service;
+
+namespace OfferOasisBackend.Controllers;
+
+[ApiController, Route("/test")]
+public class TestController : ControllerBase
+{
+    private ITestRepository TestRepository { get; init; }
+
+    public TestController(ITestRepository productRepository)
+    {
+        TestRepository = productRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(TestRepository.GetAll());
+    }
+    
+    [HttpPost]
+    public IActionResult AddParams(string name, int rating, string picture, int price, string description)
+    {
+        TestRepository.Add(new Test(0, name, rating, picture, price, description));
+        return Ok($"Test {name} has been added to DB.");
+    }
+}
