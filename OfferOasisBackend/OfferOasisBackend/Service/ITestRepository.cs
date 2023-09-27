294a4f289b979a7c2829069acd3d4cceb63244c4
rekamakadi <r.makadi@gmail.com>
2023-09-27 14:36:28 +0200

    feat: create ITestRepository.cs

---
+++ OfferOasisBackend/OfferOasisBackend/Service/ITestRepository.cs
@@ -0,0 +1,9 @@
+﻿using OfferOasisBackend.Model;
+using OfferOasisBackend.Models;
+
+namespace OfferOasisBackend.Service;
+
+public interface ITestRepository : IGenericRepository<Test>
+{
+    public string TestGetAllProducts();
+}
