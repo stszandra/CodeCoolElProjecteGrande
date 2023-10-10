9976021d66cee2c4be5f2eb6fb6dfa99f9f847e4
rekamakadi <r.makadi@gmail.com>
2023-10-10 13:21:34 +0200

    feat: clean code + add authentication

--- OfferOasisBackend/OfferOasisBackend/Program.cs
+++ OfferOasisBackend/OfferOasisBackend/Program.cs
@@ -1,39 +1,20 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using OfferOasisBackend.Controllers;
 using OfferOasisBackend.Model;
 using OfferOasisBackend.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// builder.Services.AddTransient<ITestRepository, ProductRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddSingleton<ILogger<UserController>, Logger<UserController>>();
-builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
-builder.Services.AddSingleton<ITestRepository, TestRepository>();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("ReactAppPolicy", builder =>
-    {
-        builder
-            .WithOrigins("*")
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-        
-    });
-});
-
-
-// Other service configurations
-
+AddServices();
+AddCors();
+ConfigureSwagger();
+AddAuthentication();
 
 var app = builder.Build();
+
 app.UseCors("ReactAppPolicy");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -42,11 +23,63 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
 //app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "apiWithAuthBackend",
+                ValidAudience = "apiWithAuthBackend",
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes("!SomethingSecret!")
+                ),
+            };
+        });
+}
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddSingleton<ILogger<UserController>, Logger<UserController>>();
+    builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
+    builder.Services.AddSingleton<ITestRepository, TestRepository>();
+}
+
+void AddCors()
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("ReactAppPolicy", builder =>
+        {
+            builder
+                .WithOrigins("*")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        
+        });
+    });
+}
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen();
+
+}
