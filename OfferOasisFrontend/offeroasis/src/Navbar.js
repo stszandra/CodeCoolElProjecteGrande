d77c4f92b2141b9b0950276e1259a7ed08d28f61
Mondi18 <vargamondi@gmail.com>
2023-10-11 14:22:52 +0200

    feat:refactor Navbar menus

--- OfferOasisFrontend/offeroasis/src/Navbar.js
+++ OfferOasisFrontend/offeroasis/src/Navbar.js
@@ -1,68 +1,41 @@
-import React, { useState } from "react";
-import "../src/Navbar.css"
+import React from 'react'
+// import "./navbar.css"
+import { Outlet } from 'react-router-dom'
 
-function Navbar() {
-  const [active, setActive] = useState("nav__menu");
-  const [icon, setIcon] = useState("nav__toggler");
-  const navToggle = () => {
-    if (active === "nav__menu") {
-      setActive("nav__menu nav__active");
-    } else setActive("nav__menu");
-
-    // Icon Toggler
-    if (icon === "nav__toggler") {
-      setIcon("nav__toggler toggle");
-    } else setIcon("nav__toggler");
-  };
+export default function Navbar({ ...props }) {
   return (
-    <nav className="nav">
-      <a href="#" className="nav__brand">
-      OfferOasis
-      </a>
-      <ul className={active}>
-        <li className="nav__item">
-          <a href="/home" className="nav__link">
-            Home
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="/about" className="nav__link">
-            About
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="/products" className="nav__link">
-         Endpoints
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="/account" className="nav__link">
-           Account
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="/test" className="nav__link">
-            Test
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="/contact" className="nav__link">
-            Contact
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="/information" className="nav__link">
-          Shipping information
-          </a>
-        </li>
-      </ul>
-      <div onClick={navToggle} className={icon}>
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-    </nav>
-  );
-}
+	<>
+<nav class="bg-red-50 shadow shadow-red-500 w-100 px-8 md:px-auto">
+	<div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
 
-export default Navbar;
+		<div class="text-indigo-500 md:order-1">
+			
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+				stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+					d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+			</svg>
+		</div>
+		<div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
+			<ul class="flex font-semibold justify-between">
+              
+				<li class="md:px-4 md:py-2 text-indigo-500"><a href="/about">OfferOasis</a></li>
+				<li class="md:px-4 md:py-2 hover:text-indigo-400"><a href="/products">Products</a></li>
+				<li class="md:px-4 md:py-2 hover:text-indigo-400"><a href="/information">My Orders</a></li>
+				<li class="md:px-4 md:py-2 hover:text-indigo-400"><a href="/contact">Contact</a></li>
+			</ul>
+		</div>
+		<div class="order-2 md:order-3">
+			<button class="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
+                </svg>
+              <a href="/login"><span>Login/Register</span></a>
+            </button>
+		</div>
+	</div>
+</nav>
+  <Outlet/>
+		</>
+  )
+}   
