1a8a0b1bafc645db2552a5f0a98d31e10f7decb4
rekamakadi <r.makadi@gmail.com>
2023-10-11 13:25:57 +0200

    feat: rcc + AddRoles + AddAdmin + ConfigureSwagger + AddIdentity + AddDbContext + change AddServices

--- OfferOasisBackend/OfferOasisBackend/Program.cs
+++ OfferOasisBackend/OfferOasisBackend/Program.cs
@@ -1,17 +1,22 @@
 
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OfferOasisBackend.Controllers;
-using OfferOasisBackend.Model;
+using OfferOasisBackend.Data;
 using OfferOasisBackend.Service;
+using OfferOasisBackend.Service.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 AddServices();
 AddCors();
 ConfigureSwagger();
+AddDbContext();
 AddAuthentication();
+AddIdentity();
 
 var app = builder.Build();
 
@@ -28,6 +33,9 @@ if (app.Environment.IsDevelopment())
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+AddRoles();
+AddAdmin();
 
 app.Run();
 
@@ -57,10 +65,14 @@ void AddServices()
 {
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddSingleton<IUserRepository, UserRepository>();
     builder.Services.AddSingleton<ILogger<UserController>, Logger<UserController>>();
     builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
+    builder.Services.AddSingleton<IOrderDetailsRepository, OrderDetailsRepository>();
+    builder.Services.AddSingleton<IProductRepository, ProductRepository>();
     builder.Services.AddSingleton<ITestRepository, TestRepository>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
 }
 
 void AddCors()
@@ -80,6 +92,99 @@ void AddCors()
 
 void ConfigureSwagger()
 {
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    });
+}
 
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddRoles<IdentityRole>() //Enable Identity roles 
+        .AddEntityFrameworkStores<UsersContext>();
+}
+
+void AddDbContext()
+{
+    builder.Services.AddDbContext<UsersContext>();
+    builder.Services.AddDbContext<OasisContext>();
+}
+
+void AddRoles()
+{
+    using var scope = app.Services.CreateScope();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var tAdmin = CreateAdminRole(roleManager);
+    tAdmin.Wait();
+
+    var tUser = CreateUserRole(roleManager);
+    tUser.Wait();
+}
+
+async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole("Admin")); //The role string should better be stored as a constant or a value in appsettings
+}
+
+async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole("User")); //The role string should better be stored as a constant or a value in appsettings
+}
+
+void AddAdmin()
+{
+    var tAdmin = CreateAdminIfNotExists();
+    tAdmin.Wait();
+}
+
+async Task CreateAdminIfNotExists()
+{
+    using var scope = app.Services.CreateScope();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var adminInDb = await userManager.FindByEmailAsync("admin@admin.com");
+    if (adminInDb == null)
+    {
+        var admin = new IdentityUser { UserName = "admin", Email = "admin@admin.com" };
+        var adminCreated = await userManager.CreateAsync(admin, "admin123");
+
+        if (adminCreated.Succeeded)
+        {
+            await userManager.AddToRoleAsync(admin, "Admin");
+        }
+    }
 }
