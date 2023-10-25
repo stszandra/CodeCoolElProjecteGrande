c052800b5ebeda147727e87618ef4dcf7b50c6e5
rekamakadi <r.makadi@gmail.com>
2023-10-25 11:28:33 +0200

    feat: implement layout of database structure

--- OfferOasisBackend/OfferOasisBackend/Data/OasisContext.cs
+++ OfferOasisBackend/OfferOasisBackend/Data/OasisContext.cs
@@ -6,20 +6,16 @@ namespace OfferOasisBackend.Data;
 
 public class OasisContext : DbContext
 {
-    // public DbSet<Product> Products { get; set; }
-    // public DbSet<Order> Orders { get; set; }
-    // public DbSet<Rating> Ratings { get; set; }
-
-    public DbSet<Test> Test {get; set; }
-    
-
+    public DbSet<Product?> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // TODO: It would be a good idea to move the connection string to user secrets
 
         optionsBuilder.UseSqlServer(
-            "Server=localhost,1433;Database=testDB;User Id=sa;Password=Kiskutyafüle32!;TrustServerCertificate=True;");
+            "Server=localhost,1433;Database=OfferOasisDB;User Id=sa;Password=Kiskutyafüle32!;TrustServerCertificate=True;");
     }
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -28,39 +24,21 @@ public class OasisContext : DbContext
             .IsUnique();
         
         builder.Entity<Product>()
-            .HasData(
-                new Product(1,"PS3",ProductType.Konzol,1500,10)
-            );
+            .HasData(new Product(1,"PS3",ProductType.Konzol,1500,10));
         
         builder.Entity<Order>()
             .HasIndex(u => u.Id)
             .IsUnique();
         
         builder.Entity<Order>()
-            .HasData(
-                new Order(1,1,"ABC","DGS",ShippingType.Delivery,4500)
-            );
+            .HasData(new Order(1,1,"ABC","DGS",ShippingType.Delivery,4500));
                       
         builder.Entity<OrderDetails>()
             .HasIndex(r => r.OrderDetailsId)
             .IsUnique();
 
         builder.Entity<OrderDetails>()
-            .HasData(
-                new OrderDetails(1,1,1,10)
-                );
-
-
-
-
-
-        // builder.Entity<Test>()
-        //    .HasIndex(t => t.Id)
-        //  .IsUnique();
-
-
-
+            .HasData(new OrderDetails(1,1,1,10));
     }
-    
 }
 
