50f25db97a257d07c588e4035b95138b5a3d5bb0
Mondi18 <vargamondi@gmail.com>
2023-10-26 11:43:56 +0200

    fixed:included required input fields

--- OfferOasisFrontend/offeroasis/src/Contact.js
+++ OfferOasisFrontend/offeroasis/src/Contact.js
@@ -26,6 +26,7 @@ export default function Contact() {
                     type="text"
                     id="name"
                     name="name"
+                    required
                     className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
@@ -42,6 +43,7 @@ export default function Contact() {
                     type="email"
                     id="email"
                     name="email"
+                    required
                     className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
