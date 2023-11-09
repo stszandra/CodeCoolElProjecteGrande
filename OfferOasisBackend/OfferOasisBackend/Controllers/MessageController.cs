e8598fdf0ff53d41ae9ffe318d088534403bed68
Mondi18 <vargamondi@gmail.com>
2023-11-09 11:01:26 +0100

    feat:implement MessageController

---
+++ OfferOasisBackend/OfferOasisBackend/Controllers/MessageController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using OfferOasisBackend.Model;
+using OfferOasisBackend.Models;
+using OfferOasisBackend.Service.Message;
+
+
+namespace OfferOasisBackend.Controllers;
+[ApiController, Route("/message")]
+
+public class MessageController : ControllerBase
+{
+    private readonly ILogger<ProductController> _logger;
+    private readonly IMessageRepository _messageRepository;
+    
+    public MessageController(ILogger<ProductController> logger, IMessageRepository messageRepository)
+    {
+        _logger = logger;
+        _messageRepository = messageRepository;
+    }
+    [HttpPost("/add")]
+    
+    
+    public async Task<IActionResult> AddMessage(Message message)
+    {
+        var success = await _messageRepository.Add(message);
+        if (success)
+        {
+            _logger.LogInformation($"Created message: {message.Content}");
+            return CreatedAtAction(nameof(AddMessage), new { id = message.Id }, message);
+        }
+        _logger.LogError($"Could not create message {message.Content}");
+        return NotFound($"Could not create message {message.Content}"); 
+    }
+}
