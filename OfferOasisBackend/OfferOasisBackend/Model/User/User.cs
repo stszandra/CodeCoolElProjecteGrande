24b02dd5198125a4a46a9e3161bebfbc57463fd8
Mondi18 <vargamondi@gmail.com>
2023-11-09 10:59:04 +0100

    chore:clean code

--- OfferOasisBackend/OfferOasisBackend/Model/User/User.cs
+++ OfferOasisBackend/OfferOasisBackend/Model/User/User.cs
@@ -10,7 +10,6 @@ public abstract class User
     private string Address { get; }
     private string Password { get; }
     private string Email { get; }
-    
     protected User(int id, string firstName, string lastName, string address, string password, string email)
     {
         Id = id;
@@ -19,20 +18,5 @@ public abstract class User
         Address = address;
         Password = password;
         Email = email;
-        
     }
-    
-    
-    // private static bool IsValidEmail(string email)
-    // {
-    //     string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-		  //
-    //     if (string.IsNullOrEmpty(email))
-    //         return false;
-		  //
-    //     Regex regex = new Regex(emailPattern);
-    //     return regex.IsMatch(email);
-    // }
-    
-    // ... common methods?
 }
