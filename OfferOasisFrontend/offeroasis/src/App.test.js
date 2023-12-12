b4bc09ff357dba6bbcc7b0be0f826df7bbb66051
Mondi18 <vargamondi@gmail.com>
2023-12-12 11:36:24 +0100

    chore:change filestructure and import paths

--- OfferOasisFrontend/offeroasis/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import About from './About';
-
-
-test('Please register first before you can place an order', () => {
-  render(<About />);
-  const textElements = screen.getAllByText(/Please register first before you can place an order/i);
-  expect(textElements.length).toBeGreaterThan(0);
-});
--- OfferOasisFrontend/offeroasis/src/index.js
+++ OfferOasisFrontend/offeroasis/src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
-import Navbar from "./Navbar";
-import App from "./App"
-import Login from "./Login";
-import Register from "./Register";
-import Contact from "./Contact";
-import About from "./About";
-import OrderDetails from "./OrderDetails";
-import Terms from "./Terms";
+import Navbar from "./Pages/Navbar";
+import App from "./Pages/App";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import OrderDetails from "./Pages/OrderDetails";
+import Terms from "./Pages/Terms";
 
 const router = createBrowserRouter([
   {
@@ -55,7 +54,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
