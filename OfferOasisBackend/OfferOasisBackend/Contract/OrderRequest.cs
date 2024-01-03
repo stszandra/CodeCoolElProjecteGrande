4294d8c6c46258b8332c0303905e50f41d7e75ef
rekamakadi <r.makadi@gmail.com>
2024-01-03 13:30:14 +0100

    feat: create OrderRequest.cs

---
+++ OfferOasisBackend/OfferOasisBackend/Contract/OrderRequest.cs
@@ -0,0 +1,6 @@
+﻿using OfferOasisBackend.Model;
+using OfferOasisBackend.Models;
+
+namespace OfferOasisBackend.Contract;
+
+public record OrderRequest(List<OrderDetail> ListOfOrderDetails, ShippingType ShippingType, string UserId, string ShippingAddress, string BillingAddress);
