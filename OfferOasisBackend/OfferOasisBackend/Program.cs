34eaa4a7f4448bd22672706a79f5b4e631a916f6
rekamakadi <r.makadi@gmail.com>
2024-01-03 13:39:34 +0100

    chore: fix naming convention

--- OfferOasisBackend/OfferOasisBackend/Program.cs
+++ OfferOasisBackend/OfferOasisBackend/Program.cs
@@ -10,7 +10,7 @@ using OfferOasisBackend.Data;
 using OfferOasisBackend.Model;
 using OfferOasisBackend.Service;
 using OfferOasisBackend.Service.Authentication;
-using OfferOasisBackend.Service.Message;
+using OfferOasisBackend.Service.Repository.Message;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -72,7 +72,7 @@ void AddServices()
     builder.Services.AddSingleton<ILogger<UserController>, Logger<UserController>>();
     builder.Services.AddSingleton<ILogger<ProductController>, Logger<ProductController>>();
     builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-    builder.Services.AddScoped<IOrderDetailsRepository, OrderDetailsRepository>();
+    builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
     builder.Services.AddScoped<IProductRepository, ProductRepository>();
     builder.Services.AddSingleton<ILogger<MessageController>, Logger<MessageController>>();
     builder.Services.AddScoped<IMessageRepository, MessageRepository>();
