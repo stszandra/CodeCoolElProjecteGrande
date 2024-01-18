1a7ff543a21e7599d4d7fc6693d76a56dadeee80
Mondi18 <vargamondi@gmail.com>
2024-01-18 14:07:22 +0100

    feat:conditional login prompt, styling aligment of cards,remove comments

--- OfferOasisFrontend/offeroasis/src/Pages/App.js
+++ OfferOasisFrontend/offeroasis/src/Pages/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const token=localStorage.getItem("token");
 
   useEffect(() => {
     fetch("https://localhost:7193/products")
@@ -11,50 +12,28 @@ function App() {
       .then(data => {
         data.forEach(product => {
           product.quantity = 1;
-          setProducts(data)})
+          setProducts(data)
         })
-      
-      
+      })
   }, [])
 
-  //console.log(products);
-
   const addProductsToCart = (product) => {
-    //product.quantity++;
-    console.log(product);
 
     if (localStorage.getItem('products') === null) {
-      // product.quantity++
       let cart = [];
       cart.push(product);
       localStorage.setItem('products', JSON.stringify(cart));
-      console.log(cart);
     }
     else {
       let cart = JSON.parse(localStorage.getItem('products'));
-      
-      let productAlreadyInCart = cart.find(p => p.id == product.id)
+      let productAlreadyInCart = cart.find(p => p.id === product.id)
 
-      if(productAlreadyInCart != undefined){
+      if (productAlreadyInCart !== undefined) {
         productAlreadyInCart.quantity++;
       } else {
         cart.push(product);
       }
-
-
-      // for (let i = 0; i < cart.length; i++) {
-      //   if (cart[i].id==product.id) 
-      //   {
-      //   cart[i].quantity=cart[i].quantity+1;
-      //   } 
-      //   else 
-      //   {
-      //     cart.push(product);
-      //   }
-      //}
-
       localStorage.setItem('products', JSON.stringify(cart));
-      console.log(cart);
     }
     toast(`${product.name} succesfully added to the cart!`);
 
@@ -62,28 +41,34 @@ function App() {
   return (
     products.length > 0 ? (
       <div>
+         {token ? (
+              <></>
+            ) : (
+              <h2 className='mt-8 ml-10 font-bold text-transparent bg-clip-text bg-gradient-to-r to-green-900 from-red-900 md:underline-red  text-center text-xl' >
+                Please login to add product to your cart!
+              </h2>
+            )}
         <div>
-          <section className="cards">
+          <section className="cards grid grid-cols-3 ml-12 mt-12 ">
             {products.map(p =>
-              <div className="max-w-sm  bg-white border justify-center flex-1 item-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={p.id}>
-                <div>
-                  <img className="rounded-t-lg w-1/2 h-1/2 " src={p.imageUrl} alt="" />
+              <div className="max-w-sm mb-6 w-64 bg-white border justify-center flex-col flex item-center border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={p.id}>
+                <div className="flex justify-center" >
+                  <img className="rounded-t-lg w-36 h-36" src={p.imageUrl} alt=""  />
                 </div>
-                <div className="p-5">
-                  <div className="font-bold" >
-                    <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white "></div>
-                  </div>{p.name}
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{p.productType}</p>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{p.price} Euro</p>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{p.quantity} in stock</p>
-                  <div>
-                    <button onClick={() => addProductsToCart(p)} className=" bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600"> Add to Cart</button>
-                    <ToastContainer />
+                <div className="p-5 h-1/2 content-between">
+                  <div className="font-bold " >
+                    <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-center">{p.name}</div>
                   </div>
-
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-center">{p.price} Euro</p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 flex justify-center">{p.quantityInStock} in stock</p>
+                  {token ?
+                    (<div>
+                      <button onClick={() => addProductsToCart(p)} className=" rounded-lg mb-2 bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 ml-8" > Add to Cart</button>
+                      <ToastContainer />
+                    </div>) : <div> </div>
+                  }
                 </div>
               </div>
-
             )}
           </section>
         </div>
