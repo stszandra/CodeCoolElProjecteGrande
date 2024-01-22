827d2589b816952e0bdff047aaf3ddf2e1d89b76
rekamakadi <r.makadi@gmail.com>
2024-01-22 11:39:24 +0100

    feat: create repository with methods for Cart

---
+++ OfferOasisBackend/OfferOasisBackend/Service/Repository/Cart/CartRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfferOasisBackend.Data;
+using OfferOasisBackend.Model;
+
+namespace OfferOasisBackend.Service;
+
+public class CartRepository : ICartRepository
+{
+    private readonly OasisContext _oasisContext;
+
+    public CartRepository(OasisContext oasisContext)
+    {
+        _oasisContext = oasisContext;
+    }
+
+    public async Task<CartDetail?> GetById(int id)
+    {
+        return await _oasisContext.CartDetails.FindAsync(id);
+    }
+    
+    public async Task<IEnumerable<CartDetail?>> GetAll()
+    {
+        return await _oasisContext.CartDetails.ToListAsync();
+    }
+    
+    public async Task<bool> Add(CartDetail cartDetail)
+    {
+        bool success = true;
+        try
+        {
+            await _oasisContext.AddAsync(cartDetail);
+            await _oasisContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            success = false;
+        }
+
+        return success;
+    }
+    
+    public async Task<CartDetail?> Remove(int id)
+    {
+     
+        var cartDetailToRemove = await GetById(id);
+
+        if (cartDetailToRemove == null) return cartDetailToRemove;
+        _oasisContext.Remove(cartDetailToRemove);
+        await _oasisContext.SaveChangesAsync();
+
+        return cartDetailToRemove;
+    }
+
+    public async Task<IEnumerable<CartDetail?>> GetCartOfUser(string userId)
+    {
+        return await _oasisContext.CartDetails.Where(c => c.UserId == userId).ToListAsync();
+    }
+}
---
+++ OfferOasisBackend/OfferOasisBackend/Service/Repository/Cart/ICartRepository.cs
@@ -0,0 +1,8 @@
+﻿using OfferOasisBackend.Model;
+
+namespace OfferOasisBackend.Service;
+
+public interface ICartRepository : IGenericRepository<CartDetail>
+{
+    public Task<IEnumerable<CartDetail?>> GetCartOfUser(string userId);
+}
