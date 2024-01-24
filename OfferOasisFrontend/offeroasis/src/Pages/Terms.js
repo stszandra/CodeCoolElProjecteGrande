600e19dad4939934480359d90b975e8bc080c87b
Mondi18 <vargamondi@gmail.com>
2024-01-24 11:32:30 +0100

    clean:refactor Terms page

--- OfferOasisFrontend/offeroasis/src/Pages/Terms.js
+++ OfferOasisFrontend/offeroasis/src/Pages/Terms.js
@@ -7,25 +7,19 @@ export default function Terms () {
         <h1 class="mb-4 text-4xl  flex-1text-center px-40 py-2 m-12 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">TERMS</h1>
  <div class="flex-1text-center px-40 py-2 m-12 italic ">
  <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-5xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-red-400">Welcome to</span> OfferOasis</h1>
-
 These terms and conditions outline the rules and regulations for the use of offeroasis's Website, located at nn.
-
-By accessing this website we assume you accept these terms and conditions. Do not continue to use none if you do not agree to take all of the terms and conditions stated on this page.
-
-The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of hu. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same. </div>
-
-   
+By accessing this website we assume you accept these terms and conditions. 
+Do not continue to use none if you do not agree to take all of the terms and conditions stated on this page.
+The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: 
+"Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of hu. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same. 
+</div>   
     </div>
     <h1 class="mb-4 text-4xl  flex-1text-center px-40 py-2 m-12 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Cookies</h1>
     <div class="flex-1text-center px-40 py-2 m-12 italic ">
-
  We employ the use of cookies. By accessing none, you agreed to use cookies in agreement with the offeroasis's Privacy Policy.
-
 Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies. </div>
 <h1 class="mb-4 text-4xl  flex-1text-center px-40 py-2 m-12 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">License</h1>
-   
 <div class="flex-1text-center px-40 py-2 m-12 italic ">
-
 Unless otherwise stated, offeroasis and/or its licensors own the intellectual property rights for all material on none. All intellectual property rights are reserved. You may access this from none for your own personal use subjected to restrictions set in these terms and conditions.
 Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. offeroasis does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of offeroasis,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, offeroasis shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</div>
     </>
