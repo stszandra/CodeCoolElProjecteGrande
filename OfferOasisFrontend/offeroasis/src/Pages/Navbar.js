7fb9c6ea25601e386699c3aa86bdbe586b682add
Mondi18 <vargamondi@gmail.com>
2024-01-30 14:13:26 +0100

    feat:create Sidebar,globalContext

--- OfferOasisFrontend/offeroasis/src/Pages/Navbar.js
+++ OfferOasisFrontend/offeroasis/src/Pages/Navbar.js
@@ -1,21 +1,26 @@
-import {Outlet, useLocation, useNavigate} from 'react-router-dom'
-import { useEffect,useState } from 'react';
-import {toast} from "react-toastify"; 
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState, createContext } from 'react';
+import { toast } from "react-toastify";
+import { Sidebar } from '../Components/Sidebar';
 
-export default function Navbar({ ...props }) {
+export const globalContext = createContext(null)
+
+export function Navbar({ ...props }) {
   const [token, setToken] = useState(localStorage.getItem("token"));
-  const name=localStorage.getItem("userName");
+  const [productType, setProductType] = useState(null)
+  const name = localStorage.getItem("userName");
   const navigate = useNavigate();
   const location = useLocation();
+
   
+
   useEffect(() => {
     setToken(localStorage.getItem('token'))
   }, [location]);
 
   const logout = async (event) => {
     event.preventDefault();
-    if (localStorage.getItem('productsInCart') != null)
-    {
+    if (localStorage.getItem('productsInCart') != null) {
       await saveCartToDb();
     }
     await localStorage.removeItem("token");
@@ -24,19 +29,19 @@ export default function Navbar({ ...props }) {
   }
 
   const saveCartToDb = async () => {
-   try {
+    try {
       const response = await fetch(`https://localhost:7193/cart`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(
-                {
-                  listOfCartDetails: JSON.parse(localStorage.getItem('productsInCart')).map(p => ({ Id: 0, productId: p.id, userId: localStorage.getItem('userId'), quantity: p.quantity, productPrice: p.price })),
-                }
-            )
-          });
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(
+            {
+              listOfCartDetails: JSON.parse(localStorage.getItem('productsInCart')).map(p => ({ Id: 0, productId: p.id, userId: localStorage.getItem('userId'), quantity: p.quantity, productPrice: p.price })),
+            }
+          )
+        });
 
       if (response.ok) {
         toast('You successfully saved your cart');
@@ -47,16 +52,14 @@ export default function Navbar({ ...props }) {
       console.error('An error occurred:', error);
     }
   };
-  
+
   return (
-    <>
+    <globalContext.Provider value={{productType, setProductType}}>
       <nav className="bg-red-50 shadow shadow-red-500 w-100 px-8 md:px-auto">
         <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
           <div className="text-indigo-500 md:order-1">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
+            <svg xmlns="" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
               stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
             </svg>
           </div>
           <div className="text-gray-500 order-3 w-full md:w-auto md:order-2 flex items-center">
@@ -69,27 +72,28 @@ export default function Navbar({ ...props }) {
           </div>
           <div className="order-2 md:order-3 flex items-center">
 
-  <div className="flex items-center gap-4">
-    {token?
-    <h4 className="text-gray-500">Logged in as {name}</h4>
-: <></>  }
+            <div className="flex items-center gap-4">
+              {token ?
+                <h4 className="text-gray-500">Logged in as {name}</h4>
+                : <></>}
 
-    <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-        <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
-      </svg>
-      
-      {token ? (
-        <a href="/login" onClick={(e) => logout(e)}><span>Logout</span></a>
-      ) : (
-        <a href="/login" ><span>Login/Register</span></a>
-      )}
-    </button>
-  </div>
-</div>
-  </div>
+              <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
+                </svg>
+
+                {token ? (
+                  <a href="/login" onClick={(e) => logout(e)}><span>Logout</span></a>
+                ) : (
+                  <a href="/login" ><span>Login/Register</span></a>
+                )}
+              </button>
+            </div>
+          </div>
+          <Sidebar />
+        </div>
       </nav>
-      <Outlet/>
+      <Outlet />
       <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="
@@ -112,6 +116,6 @@ export default function Navbar({ ...props }) {
         </div>
       </footer>
 
-    </>
+    </globalContext.Provider>
   )
 }   
