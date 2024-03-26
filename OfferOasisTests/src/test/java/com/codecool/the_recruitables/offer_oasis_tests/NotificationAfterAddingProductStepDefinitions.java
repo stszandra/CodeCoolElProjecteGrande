436992e97da0fe61faea628a46e48f8881aee12e
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-03-26 18:02:08 +0100

    feat: create steps for adding product to cart

--- OfferOasisTests/src/test/java/com/codecool/the_recruitables/offer_oasis_tests/NotificationAfterAddingProductStepDefinitions.java
+++ OfferOasisTests/src/test/java/com/codecool/the_recruitables/offer_oasis_tests/NotificationAfterAddingProductStepDefinitions.java
@@ -1,19 +1,44 @@
 package com.codecool.the_recruitables.offer_oasis_tests;
 
+import com.codecool.the_recruitables.offer_oasis_tests.pageFactory.LoginPage;
+import com.codecool.the_recruitables.offer_oasis_tests.pageFactory.ProductsPage;
+import io.cucumber.java.After;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NotificationAfterAddingProductStepDefinitions {
+    WebDriver chromedriver = new ChromeDriver();
+    ProductsPage productsPage = new ProductsPage(chromedriver);
+    LoginPage loginPage = new LoginPage(chromedriver);
+
+    @Given("I am logged in to add products to cart")
+    public void iAmLoggedInToAddProductsToCart() {
+        loginPage.loginAsAdmin();
+    }
+
     @And("I am on products page")
     public void iAmOnProductsPage() {
+        assertTrue(productsPage.verifySite());
     }
 
     @When("I click on Add to Cart button below {}")
-    public void iClickOnAddToCartButtonBelow(String arg0) {
+    public void iClickOnAddToCartButtonBelow(String productName) {
+        productsPage.addItemToCart(productName);
     }
 
     @Then("I am notified that {} is added to my cart")
-    public void iAmNotifiedThatIsAddedToMyCart(String arg0) {
+    public void iAmNotifiedThatIsAddedToMyCart(String productName) {
+        assertTrue(productsPage.ISeeToastForAddingToCart(productName));
+    }
+
+    @After
+    public void tearDown() {
+        chromedriver.quit();
     }
 }
