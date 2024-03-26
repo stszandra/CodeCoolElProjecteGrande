e74fe65098ed5144b58c10349dad75c2958d41ac
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2024-03-26 14:32:05 +0100

    fix: add id to login button

--- OfferOasisFrontend/offeroasis/src/Pages/Login.js
+++ OfferOasisFrontend/offeroasis/src/Pages/Login.js
@@ -92,7 +92,7 @@ export default function Login() {
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" value={formData.password} onChange={handleChange}/>
                         </div>
-                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in to your account</button>
+                        <button id="login" type="submit" className="w-full text-white bg-blue-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in to your account</button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                             Don't have an account? <a href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Register here</a>
                         </p>
