82d8316812a417d2829914bf979d69c4c6ea5372
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2024-03-26 14:33:50 +0100

    feat: locate elements

--- OfferOasisTests/src/main/java/com/codecool/the_recruitables/offer_oasis_tests/pageFactory/LoginPage.java
+++ OfferOasisTests/src/main/java/com/codecool/the_recruitables/offer_oasis_tests/pageFactory/LoginPage.java
@@ -1,13 +1,29 @@
 package com.codecool.the_recruitables.offer_oasis_tests.pageFactory;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+import java.util.Objects;
 
 public class LoginPage {
     WebDriver driver;
 
+    String URL = "http://localhost:3000/login";
+    @FindBy(id = "email")
+    WebElement emailField;
+    @FindBy(id = "password")
+    WebElement passwordField;
+    @FindBy(id = "login")
+    WebElement loginBtn;
+    @FindBy(id = "logged-in-as")
+    WebElement userNameText;
+
     public LoginPage(WebDriver driver){
         this.driver=driver;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         PageFactory.initElements(driver, this);
     }
 }
