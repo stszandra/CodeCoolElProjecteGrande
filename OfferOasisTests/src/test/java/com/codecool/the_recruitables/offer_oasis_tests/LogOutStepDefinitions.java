cba4237d433cb15b3e2af307aa57eda4858c164d
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2024-03-26 18:00:49 +0100

    in progress: login step defined

--- OfferOasisTests/src/test/java/com/codecool/the_recruitables/offer_oasis_tests/LogOutStepDefinitions.java
+++ OfferOasisTests/src/test/java/com/codecool/the_recruitables/offer_oasis_tests/LogOutStepDefinitions.java
@@ -1,13 +1,26 @@
 package com.codecool.the_recruitables.offer_oasis_tests;
 
+import com.codecool.the_recruitables.offer_oasis_tests.pageFactory.LoginPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LogOutStepDefinitions {
+
+    public WebDriver chromedriver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(chromedriver, Duration.ofSeconds(15));
+
     @Given("I am logged in")
     public void iAmLoggedIn() {
+        LoginPage loginPage = new LoginPage(chromedriver);
+        loginPage.loginAsAdmin();
     }
 
     @When("I click Logout button")
